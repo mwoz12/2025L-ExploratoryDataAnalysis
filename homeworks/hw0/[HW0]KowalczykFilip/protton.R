9d2a40de04a7e1b8e6dc2e9d01a404265a8173fb
kowebryant <filippo.kowalczyk@gmail.com>
2025-02-25 09:40:17 +0100

    Add files via upload

---
+++ homeworks/hw0/[HW0]KowalczykFilip/protton.R
@@ -0,0 +1,10 @@
+install.packages("proton")
+library(proton)
+proton()
+for (i in 1:1000) {
+  proton(action = "login", login="johnins", password=top1000passwords[i])
+}
+
+for (i in 1:1000) {
+  proton(action = "login", login="johnins", password=top1000passwords[i])
+}
