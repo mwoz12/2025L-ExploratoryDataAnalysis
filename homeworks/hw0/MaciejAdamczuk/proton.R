87f29c0a59acdd46358972f329662d5feee46fca
AdamczukM <01183260@pw.edu.pl>
2025-02-25 10:02:03 +0100

    Add files via upload

--- homeworks/hw0/MaciejAdamczuk/proton.R
+++ homeworks/hw0/MaciejAdamczuk/proton.R
@@ -1,5 +1,12 @@
+install.packages("proton")
 library(proton)
 proton()
 
+# Problem 1 
 head(employees)
-login <- employees[employees$name == "John",employees$surname == "Insecure",3]
+login <- employees[employees$name == "John" & 
+        employees$surname == "Insecure", 
+        "login"]
+
+login 
+proton(action = "login", login=login)
