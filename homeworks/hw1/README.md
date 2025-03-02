41c238002e50a8a2544049b87d839e2b7bc06bdf
Anna Kozak <anna1993kozak@gmail.com>
2025-03-02 18:52:12 +0100

    add homework1 files

---
+++ homeworks/hw1/README.md
@@ -0,0 +1,20 @@
+### Opis kolumn w zbiorze danych
+
+| Kolumna |Opis  |
+|--|--|
+|Person ID  | identyfikator dla każdej osoby |
+|Gender| płeć osoby (mężczyzna/kobieta)|
+|Age| wiek osoby w latach|
+|Occupation|zawód lub profesja osoby|
+|Sleep Duration (godziny)|liczba godzin snu w ciągu doby|
+|Quality of Sleep (skala: 1-10)|subiektywna ocena jakości snu w zakresie od 1 do 10|
+|Physical Activity Level (minuty/dzień)|liczba minut aktywności fizycznej wykonywanej przez daną osobę dziennie|
+|Stress Level (skala: 1-10)|subiektywna ocena poziomu stresu odczuwanego przez daną osobę, w zakresie od 1 do 10|
+|BMI Category|kategoria BMI osoby (np. niedowaga, normalna, nadwaga).|
+|Blood Pressure (skurczowe/rozkurczowe)|pomiar ciśnienia krwi danej osoby, wskazany jako przewaga ciśnienia skurczowego nad rozkurczowym|
+|Heart Rate (bpm)|tętno spoczynkowe danej osoby w uderzeniach na minutę|
+|Daily Steps|liczba kroków wykonywanych przez daną osobę w ciągu dnia|
+|Sleep Disorder|obecność lub brak zaburzeń snu u danej osoby (brak, bezsenność, bezdech senny)|
+
+
+
---
+++ homeworks/hw1/hw1.R
@@ -0,0 +1,71 @@
+# Ładowanie pakietów
+library(dplyr)
+
+
+# Ładowanie danych
+df <- read.csv("data.csv")
+
+
+########### Zad 1 (1pkt) ###########
+# Określ jakiego typu są poniższe zmienne w zbiorze danych.
+
+# Rozwiązanie: ------------------------------------------------------------
+
+# Gender - 
+# Age - 
+# Occupation - 
+# Sleep.Duration - 
+# Quality.of.Sleep -
+# Physical.Activity.Level - 
+# BMI.Category - 
+# Sleep.Disorder -
+
+
+########### Zad 2 (0.5pkt) ###########
+# Jakie mają średie tętno ludzie w różnych kategoriach BMI? Dla jakiej kategorii BMI średnie tętno jest najwyższe?
+
+# Rozwiązanie: ------------------------------------------------------------
+
+
+########### Zad 3 (0.5pkt) ###########
+# W jakim zawodzie ludzie średnio najwięcej śpią jeżeli ich poziom stresu jest poniżej 7?
+
+# Rozwiązanie: ------------------------------------------------------------
+
+
+########### Zad 4 (0.5pkt) ###########
+# W jakich zawodach najcześćiej pracują osoby z dowolnym zaburzeniem snu? 
+
+# Rozwiązanie: ------------------------------------------------------------
+
+
+########### Zad 5 (0.5pkt) ###########
+# W jakich zawodach jest więcej otyłych (Obese) mężczyzn niż otyłych kobiet?
+
+# Rozwiązanie: ------------------------------------------------------------
+
+
+########### Zad 6 (1 pkt) ###########
+# Znajdź 3 zawody dla których w naszym zbiorze mamy więcej niż 20 przykładów oraz 
+# różnica między ciśnieniem skurczowym i rozkurczowym jest średnio największa.
+# Następnie dla zawodu, który jest drugi pod względem średniej różnicy i ma ponad 20 przykładów
+# podaj średnią i medianę czasu snu dla różnych jakości snu.
+
+# Rozwiązanie: ------------------------------------------------------------
+
+
+########### Zad 7 (1 pkt) ###########
+# Znajdź 3 zawody, w których ludzie średnio najbardziej się ruszają. Jaka jest różnica
+# między średnim tętnem z wszystkich danych a średnim tętnem w tych zawodach grupując dane
+# względem osób które mają co najmniej 50 lat (grupa 1) i poniżej 50 lat (grupa 2). 
+
+# Rozwiązanie: ------------------------------------------------------------
+
+
+########### Zad 8 (1 pkt) ###########
+# Pogrupuj ludzi względem wieku przypisując ich do kubełków wieku co 10 (czyli [0,10), [10, 20) etc.).
+# Następnie podaj w każdej z tych grup najbardziej popularny zawód dla mężczyzn i kobiet
+# Dodatkowo wyświetl średnią, medianę i odchylenie standardowe dla poziomu stresu w tak utworzonych grupach.
+
+# Rozwiązanie: ------------------------------------------------------------
+
